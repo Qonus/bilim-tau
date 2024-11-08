4940a4a737374e6198e02031d922ae5d44e93aa5
Qonus <94901082+Qonus@users.noreply.github.com>
2024-11-09 02:31:03 +0500

    [Added] MaterialPostForm with some functionality

---
+++ src/app/postmaterial/page.tsx
@@ -0,0 +1,10 @@
+import MaterialPostForm from "../../../components/MaterialPostFormComponent/MaterialPostForm";
+import Profile from "../../../components/ProfileComponent/Profile";
+
+export default function PostMaterial() {
+  return (
+    <>
+      <MaterialPostForm />
+    </>
+  );
+}
