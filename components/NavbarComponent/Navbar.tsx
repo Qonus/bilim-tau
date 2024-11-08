1bac6d30fc15c00d4f3f2d3bf8e0280b3f126421
Qonus <94901082+Qonus@users.noreply.github.com>
2024-11-08 23:01:38 +0500

    Update Navbar.tsx

--- components/NavbarComponent/Navbar.tsx
+++ components/NavbarComponent/Navbar.tsx
@@ -1,8 +1,8 @@
-'use client'
-import { useState } from 'react';
-import Link from 'next/link';
-import styles from './Navbar.module.scss';
-import Image from 'next/image';
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import styles from "./Navbar.module.scss";
+import Image from "next/image";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -17,13 +17,26 @@ const Navbar = () => {
         <Link href="/" className={styles.logo}>
           MyWebsite
         </Link>
-        
+
         {/* Desktop Menu */}
         <div className={styles.navLinks}>
-          <Link href="/" style={{textDecoration:'none', color:'white'}}>Главная</Link>
-          <Link href="/materials" style={{textDecoration:'none', color:'white'}}>Материалы</Link>
+          <Link href="/" style={{ textDecoration: "none", color: "white" }}>
+            Главная
+          </Link>
+          <Link
+            href="/materials"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Материалы
+          </Link>
+          <Link
+            href="/profile"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Профиль
+          </Link>
         </div>
-        
+
         {/* Mobile Menu Button */}
         <button className={styles.menuButton} onClick={toggleMenu}>
           ☰
@@ -33,8 +46,27 @@ const Navbar = () => {
       {/* Dropdown Menu for Mobile */}
       {menuOpen && (
         <div className={styles.dropdownMenu}>
-          <Link href="/" style={{textDecoration:'none', color:'white'}} onClick={() => setMenuOpen(false)}>Главная</Link>
-          <Link href="/Materials" style={{textDecoration:'none', color:'white'}} onClick={() => setMenuOpen(false)}>Материалы</Link>
+          <Link
+            href="/"
+            style={{ textDecoration: "none", color: "white" }}
+            onClick={() => setMenuOpen(false)}
+          >
+            Главная
+          </Link>
+          <Link
+            href="/Materials"
+            style={{ textDecoration: "none", color: "white" }}
+            onClick={() => setMenuOpen(false)}
+          >
+            Материалы
+          </Link>
+          <Link
+            href="/profile"
+            style={{ textDecoration: "none", color: "white" }}
+            onClick={() => setMenuOpen(false)}
+          >
+            Профиль
+          </Link>
         </div>
       )}
     </nav>
