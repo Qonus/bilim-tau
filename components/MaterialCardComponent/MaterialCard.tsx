02449911482967474aa84c0c98909c9afc6de71a
Qonus <94901082+Qonus@users.noreply.github.com>
2024-11-09 12:41:08 +0500

    Update MaterialCard.tsx

--- components/MaterialCardComponent/MaterialCard.tsx
+++ components/MaterialCardComponent/MaterialCard.tsx
@@ -5,46 +5,50 @@ import Link from "next/link";
 export default function MaterialCard({
   href,
   title = "",
+  description = "",
   likes = 0,
-  image = "/default_image.jpg",
-  tags = ["Тэг Плейсхолдер", "Убрать тэги позже"]
+  tags = ["Тэг Плейсхолдер", "Убрать тэги позже"],
 }: {
   href: string;
   title?: string;
+  description?: string;
   likes?: number;
-  image?: string;
   tags?: string[];
 }) {
   return (
-    <Link href={href} className={styles.material_card + " glass"} style={{textDecoration:'none'}}>
-      <div className={styles.material_card__image_container}>
-        <Image
-          className={styles.material_card__image}
-          src={image}
-          width={600}
-          height={300}
-          alt="article image"
-        />
-      </div>
-      
+    <Link
+      href={href}
+      className={styles.material_card + " glass"}
+      style={{ textDecoration: "none" }}
+    >
       <div className={styles.material_card__text}>
-        <div className={styles.material_card__text__title}>  Заголовок  </div>
+        <div className={styles.material_card__text__title}> {title} </div>
         <div className={styles.material_card__text__description}>
-          <p>
-            Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание Описание  
-          </p>
+          <p>{description}</p>
         </div>
-        <div style={{ marginTop: '20px', display: 'flex', gap: '10px', flexWrap: 'wrap' }}>
-
-        {tags.map((tag, index) => (
-            <span key={index} style={{ padding: '5px 10px', backgroundColor: '#59a6ff', borderRadius: '4px', fontSize: '14px' }}>
+        <div
+          style={{
+            marginTop: "20px",
+            display: "flex",
+            gap: "10px",
+            flexWrap: "wrap",
+          }}
+        >
+          {tags.map((tag, index) => (
+            <span
+              key={index}
+              style={{
+                padding: "5px 10px",
+                backgroundColor: "#59a6ff",
+                borderRadius: "4px",
+                fontSize: "14px",
+              }}
+            >
               <p> {tag} </p>
             </span>
           ))}
+        </div>
       </div>
-      </div>
-
-      
     </Link>
   );
 }
