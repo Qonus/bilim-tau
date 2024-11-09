95ee28f139e5585af5216c4c8ea2e5b05f3398c5
Boiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii <bolathanzada@gmail.com>
2024-11-09 11:57:04 +0500

    [Add] author section on material page

--- components/MaterialComponent/Material.tsx
+++ components/MaterialComponent/Material.tsx
@@ -8,12 +8,23 @@ interface ArticleProps {
   publish_date: Date;
   last_changed?: Date;
   content: string;
+  author?: string;
+  author_profile?:string;
 }
 
-const Material: React.FC<ArticleProps> = ({ title, image, publish_date, last_changed, content }) => {
+const Material: React.FC<ArticleProps> = ({ title, image, publish_date, last_changed, content, author, author_profile }) => {
   return (
     <div className={styles.material}>
       <div className={styles.material__wrapper}> 
+        <div className={styles.material__author}>
+          От: <Image 
+          className={styles.material__author__profile}
+          src = {author_profile ?? "/default-profile.jpg"}
+          alt="pfp"
+          height={10}
+          width={10}/>
+          {author ?? "Аноним"}
+        </div>
         <div className={styles.material__title}><p>{title}</p></div>
         <div className={styles.material__dates}>
           Published: {publish_date.toDateString()}, Last Change: {last_changed?.toDateString()}
